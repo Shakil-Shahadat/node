56f8f9253536734506a15a54e80d7104ceb3eeb7
Shakil Shahadat <shakil.regi@gmail.com>
2020-06-10 05:41:45 +0600

    Update server.js

--- a/03/server.js
+++ a/03/server.js
@@ -3,6 +3,7 @@ let http = require( 'http' );
 let server = http.createServer( function ( req, res ) {
 	res.write( '<h1>Hello World!</h1>' );
 	res.write( '<p>Method: ' + req.method + '</p>' );
+	res.write( '<p>URL: ' + req.url + '</p>' );
 	res.end();
 });
 

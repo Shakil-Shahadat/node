a22708e7300ccdf689e143764555ac6c685293f5
Shakil Shahadat <shakil.regi@gmail.com>
2021-03-25 21:17:01 +0600

    Update server.js

--- nodebigenner/server.js
+++ nodebigenner/server.js
@@ -1,10 +1,12 @@
 const http = require( 'http' );
+let url = require( 'url' );
 
 function start()
 {
 	function onRequest( req, res )
 	{
-		console.log( 'Request received.' );
+		let pathname = url.parse( req.url ).pathname;
+		console.log( 'Request for ' + pathname + ' received.' );
 		res.writeHead( 200, { 'content-type': 'text/plain' } );
 		res.write( 'Hello World!' );
 		res.end();
